61a981efa5353dc4c846ebf7e1d9aceb02d7dbf9
Jordi Sabaté <3697530+zarpilla@users.noreply.github.com>
2025-01-28 15:46:11 +0100

    first working version

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const printMsg = () => {
-  console.log("This is a message from the demo package");
+const axios = require("axios");
+let apiBase = "https://api-p.monetr.co.uk/api/v1";
+
+const environments = {
+    production: "https://api-p.monetr.co.uk/api/v1",
+    staging: "https://api-s.monetr.co.uk/api/v1",
+    development: "http://localhost:8080/api/v1",
+}
+
+let token = null;
+const setToken = (token) => {
+  this.token = token;
 };
 
-exports = { printMsg };
+const setEnvironment = (environment) => {
+    this.apiBase = environments[environment];    
+};
+
+const report = (kpiId, kpiDimensionId, value) => {
+  try {
+    const headers = {
+      "monetr-sdk-token": this.token,
+    };
+    const apiBase = this.apiBase || environments.production;
+    axios.post(
+      `${apiBase}/kpis/${kpiId}/values/report-realtime/${kpiDimensionId}`,
+      { value },
+      { headers }
+    )
+    return { send: true };
+  } catch (e) {
+    //console.error(e);
+    return e;
+  }
+};
+
+exports.monetr = { setToken, setEnvironment, report };
